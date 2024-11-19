bd510e2609fcf0abc57f6f4a622c392741597826
Aris ALEXIA <78435539+Haeliseu@users.noreply.github.com>
2024-11-19 19:28:40 +0100

    Delete WebServer directory

--- WebServer/backend/routes/api.js
+++
@@ -1,9 +0,0 @@
-require ("dotenv").config();
-const express = require("express");
-const router = express();
-const User = require("../controllers/userController");
-
-
-router.post("/login", User.login);
-
-module.exports = router;
--- WebServer/frontend/src/components/Containers.js
+++
@@ -1,9 +0,0 @@
-const Containers = () => {
-    return (
-        <div>
-        <h1>Containers</h1>
-        </div>
-    );
-    };
-
-export default Containers;
